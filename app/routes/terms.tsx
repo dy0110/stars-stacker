e0e744f94bcbc9e910f28c41355687e600415f64
吉田大地 <yoshidadaichi@yoshidadaichinoMacBook-Air.local>
2025-11-13 23:23:57 +0900

    ページ追加 - テーマ拡張 - lucide-react導入

---
+++ app/routes/terms.tsx
@@ -0,0 +1,10 @@
+export const meta = () => {
+	return [
+		{ title: "New React Router App" },
+		{ name: "description", content: "Welcome to React Router!" },
+	];
+};
+
+export default function Term() {
+	return <div>term</div>;
+}
